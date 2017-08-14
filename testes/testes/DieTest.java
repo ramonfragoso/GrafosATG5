76ecc48ac93ca173b8fb109f3110ad667dfa4a74
Jose Ramon Fragoso da Silva 116210412 <joserfs@lcc2-32.lcc.ufcg.edu.br>
2017-08-14 16:41:09 -0300

    quase

--- testes/testes/DieTest.java
+++ testes/testes/DieTest.java
@@ -17,7 +17,9 @@ public class DieTest {
 	
 	Die dieResult1;
 	Die dieResult2;
-
+	
+	Die dieNumSidesError;
+	
 	@Before
 	public void setUp() throws Exception {
 		
@@ -27,8 +29,8 @@ public class DieTest {
 		dieNumSides1 = new Die(15);
 		dieNumSides2 = new Die(3);
 		 
-		dieResult1 = new Die(4, 3);
-		dieResult2 = new Die(5, 4);
+		dieResult1 = new Die(10, 8);
+		dieResult2 = new Die(7, 6);
 	}
 
 	@Test
@@ -46,50 +48,78 @@ public class DieTest {
 		
 		assertEquals(1, dieNumSides2.getResult()); 
 		assertEquals(1, dieNumSides2.getResult()); 
-	}
-//
-//	@Test(expected = AssertionError.class)
-//	public void testDieIntError() {
-//		
-//		 Die dieNumSidesError1 = new Die(1);
-//		 Die dieNumSidesError2 = new Die(-1);
-//	}
+	} 
 
 	@Test
 	public void testDieIntInt() {
 		
 		
-//		assertEquals(15, dieResult1.getNumSides());
-//		assertEquals(3, dieResult2.getNumSides()); 
-//		
-//		assertEquals(1, dieResult1.getResult()); 
-//		assertEquals(1, dieResult2.getResult()); 
+		assertEquals(10, dieResult1.getNumSides());
+		assertEquals(7, dieResult2.getNumSides()); 
+		
+		assertEquals(8, dieResult1.getResult()); 
+		assertEquals(6, dieResult2.getResult()); 
+	} 
+
+	@Test
+	public void testRoll() {
+		
+		int newResult = dieResult1.roll();
+		assertEquals(newResult, dieResult1.getResult());
+		
+		newResult = dieResult1.roll();
+		assertEquals(newResult, dieResult1.getResult());
+
+		newResult = dieResult1.roll();
+		assertEquals(newResult, dieResult1.getResult());
+	}
+	
+	@Test
+	public void testEqualsObject() {
+		
+		Die sixSidesDie = new Die(6, 3);
+		Die anotherSixSidesDie = new Die(6, 5);
+		assertEquals(false, sixSidesDie.equals(anotherSixSidesDie));
+	
+		Die resultFourDie = new Die(6, 4);
+		Die anotherResultFourDie = new Die(5, 4);
+		assertEquals(false, resultFourDie.equals(anotherResultFourDie));
+	
+		Die dieEqualsToMyLittleDie = new Die(8, 6);
+		Die myLittleDie = new Die(8, 6);
+		assertEquals(true, dieEqualsToMyLittleDie.equals(myLittleDie));
+		
+		Integer number = new Integer(6);
+		assertEquals(false, sixSidesDie.equals(null));
+		assertEquals(true, sixSidesDie.equals(sixSidesDie));
+		assertEquals(false, sixSidesDie.equals(number));
+	}
+
+	@Test
+	public void testToString() {
+		
+		assertEquals("Num sides 6 result 1",die1.toString());
+		assertEquals("Num sides 6 result 1",die2.toString());
+		assertEquals("Num sides 15 result 1",dieNumSides1.toString());
+		assertEquals("Num sides 3 result 1",dieNumSides2.toString());
+		assertEquals("Num sides 10 result 8",dieResult1.toString());
+		assertEquals("Num sides 7 result 6",dieResult2.toString());
 	}
 
 //	@Test
-//	public void testRoll() {
-//		fail("Not yet implemented");
+//	public void testDies() {
+//		
+//		Die die = new Die();
+//		die.getResult();
 //	}
-//
-//	@Test
-//	public void testGetNumSides() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetResult() {
+	
+//	@Test(expected = AssertionError.class)
+//	public void testDieError() {
+//		
+//		dieNumSidesError = new Die(1);
+//		dieNumSidesError = new Die(-1);
 //	
-//
+//		dieNumSidesError = new Die(1);
+//		dieNumSidesError = new Die(-1);
 //	}
-//
-//	@Test
-//	public void testEqualsObject() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testToString() {
-//		fail("Not yet implemented");
-//	}
-
 }
